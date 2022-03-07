17367e75ef4fbc15952a334014c7e21c9fb7f5a5
Dan <33081566+deobfuscate@users.noreply.github.com>
2022-03-06 21:40:08 -0800

    Create Discord.java

---
+++ src/com/github/DiscordSynergy/Discord.java
@@ -0,0 +1,17 @@
+package com.github.DiscordSynergy;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import javax.security.auth.login.LoginException;
+
+public class Discord {
+    private JDA jda;
+
+    public void login() throws LoginException {
+        jda = JDABuilder.createDefault("").build();
+    }
+
+    public void quit() {
+        jda.shutdownNow();
+    }
+}
