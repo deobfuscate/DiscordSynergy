45119edde3bf41b33fedb7f9317c5235b0bc2e9f
Dan <33081566+deobfuscate@users.noreply.github.com>
2022-03-23 23:35:32 -0700

    Display messages from Discord to Minecraft players

--- src/com/github/DiscordSynergy/Discord/MessageListener.java
+++ src/com/github/DiscordSynergy/Discord/MessageListener.java
@@ -1,5 +1,8 @@
 package com.github.DiscordSynergy.Discord;
 
+import java.util.Collection;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -7,14 +10,17 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class MessageListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        if (event.isFromType(ChannelType.PRIVATE)) {
-            System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(),
-                event.getMessage().getContentDisplay());
-        }
-        else {
+        if (event.isFromType(ChannelType.TEXT)) {
             System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
                 event.getTextChannel().getName(), event.getMember().getEffectiveName(),
                 event.getMessage().getContentDisplay());
+
+            Collection<? extends Player> players = Bukkit.getOnlinePlayers();
+            for(Player player : players)
+                player.sendMessage("[" + event.getGuild().getName() + "] [" +
+                    event.getTextChannel().getName() + "] " +
+                    event.getMember().getEffectiveName() + ": " + 
+                    event.getMessage().getContentDisplay());
         }
     }
 }
