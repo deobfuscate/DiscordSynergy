2b79271ea42938eaa88df13abbf15d580a99366b
Dan <33081566+deobfuscate@users.noreply.github.com>
2022-08-10 22:28:44 -0700

    Update ServerEvents.java

--- src/com/github/DiscordSynergy/ServerEvents.java
+++ src/com/github/DiscordSynergy/ServerEvents.java
@@ -32,8 +32,7 @@ public class ServerEvents implements Listener {
     @EventHandler
     public void onLeave(PlayerQuitEvent e) {
         if (announceQuitsToDiscord && e.getPlayer().getDisplayName() != null && e.getPlayer().getDisplayName() != "") {
-            return;
+            Connection.jda.getTextChannelById(discordChannelId).sendMessage(e.getPlayer().getDisplayName() + " left the Minecraft server").queue();
         }
-        Connection.jda.getTextChannelById(discordChannelId).sendMessage(e.getPlayer().getDisplayName() + " left the Minecraft server").queue();
     }
 }
